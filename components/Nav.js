5103cef439a46489c6e4a73ad1ddbb474ee415b4
Nya <95955389+aahna-ashina@users.noreply.github.com>
2022-01-02 00:29:19 +0400

    feat: Integrate with Web3 modal
    Display selected address

--- components/Nav.js
+++ components/Nav.js
@@ -20,8 +20,23 @@ async function connectWallet() {
   const web3Modal = new Web3Modal({
     providerOptions
   });
+  
   const provider = await web3Modal.connect();
   console.log(`provider: ${provider}`);
+  
   const web3 = new Web3(provider);
   console.log(`web3: ${web3}`);
+  
+  let address = window.web3.currentProvider.selectedAddress;
+  console.log('address ' + address);
+
+  document.getElementById('connect-wallet-button').innerHTML = address;
+
+  web3.eth.getBalance(address, function(err, result) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(web3.utils.fromWei(result, "ether") + " ETH")
+    }
+  })
 }
